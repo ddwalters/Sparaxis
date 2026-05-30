12dacb1e966cd2fdf477f711f6377e734e7d10d7
Hiileike <ddwalters987@gmail.com>
2026-05-30 15:50:15 -0400

    Basic Char animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movementInput;
 
+    private Animator animator;
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -30,10 +35,39 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputAction.CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
+
+        if (movementInput != Vector2.zero)
+        {
+            lastMoveDirection = movementInput.normalized;
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movementInput);
+        Animate();
+
+        if (movementInput.x < 0 && !facingLeft || movementInput.x > 0 && facingLeft)
+            Flip();
+    }
+
+    private void Animate()
+    {
+        if (animator != null)
+        {
+            animator.SetFloat("MoveX", movementInput.x);
+            animator.SetFloat("MoveY", movementInput.y);
+            animator.SetFloat("MoveMagnitude", movementInput.magnitude);
+            animator.SetFloat("LastMoveX", lastMoveDirection.x);
+            animator.SetFloat("LastMoveY", lastMoveDirection.y);
+        }
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
